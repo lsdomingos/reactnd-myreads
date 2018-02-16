d128c6873ef9e4405618917036b4311194d6b0f1
LuisDomingos <luis.domingos@b-i.com>
2018-02-16 11:50:26 +0000

    Adding isDisbaled porperty
    feat: Adding is Disbaled property to hide shelf selection on already
    dadded books

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -3,24 +3,27 @@ import React, { Component } from 'react'
 class BookItem extends Component {
 
   render() {
-    const { book, shelves } = this.props;
+    const { book, shelves, isDisabled } = this.props;
 
     return (
       <div className="book">
         <div className="book-top">
-        <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select value={book.shelf} onChange={(e) => this.props.updateData(book, e.target.value)}>
-              <option value="none" disabled>Move to...</option>
-              {
-                Object.keys(shelves).map((shelf, index) => (
-                  <option key={shelf + index} value={shelf} defaultValue={shelf === book.shelf}>
-                    {shelves[shelf]}
-                  </option>
-                ))
-              }
-              <option value="none" >None</option>
-            </select>
+            {!isDisabled && (
+              <select value={book.shelf} onChange={(e) => this.props.updateData(book, e.target.value)}>
+                <option value="none" disabled>Move to...</option>
+                {
+                  Object.keys(shelves).map((shelf, index) => (
+                    <option key={shelf + index} value={shelf} defaultValue={shelf === book.shelf}>
+                      {shelves[shelf]}
+                    </option>
+                  ))
+                }
+                <option value="none" >None</option>
+              </select>
+            )}
+
           </div>
         </div>
         <div className="book-title">{book.title}</div>
